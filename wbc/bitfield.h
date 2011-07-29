0de0cc380f89d51cb3681a0632474d549375caf7
Jean-Daniel Dupas <jd.dupas@ninsight.com>
2011-07-30 00:40:59 +0200

    Move bit maths in bitfield.h

--- wbc/bitfield.h
+++ wbc/bitfield.h
@@ -93,4 +93,132 @@ bool WBBitIsSet32(uint32_t value, uint32_t n) {
 #define WBFlagSet(field, value)	field = ((value) != 0) ? 1 : 0
 #define WBFlagTestAndSet(field, value) ({ bool __old = field; field = ((value) != 0) ? 1 : 0; __old; })
 
+// MARK: -
+// MARK: Bit maths
+#if defined(_MSC_VER)
+
+// FIXME: unsafe on hardware that do not support it.
+// SC_INLINE int popcnt32(uint32_t value) { return __popcnt(value); }
+// SC_INLINE int popcnt64(uint64_t value) { return __popcnt64(value); }
+
+static inline int popcount32(uint32_t x) {
+	static const uint32_t m1 = 0x55555555; //binary: 0101...
+	static const uint32_t m2 = 0x33333333; //binary: 00110011..
+	static const uint32_t m4 = 0x0f0f0f0f; //binary:  4 zeros,  4 ones ...
+	static const uint32_t h01= 0x01010101; //the sum of 256 to the power of 0,1,2,3...
+	x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
+  x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
+  x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
+  return (x * h01) >> 24;  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24)
+}
+
+static inline int popcount64(uint64_t x) {
+	static const uint64_t m1  = 0x5555555555555555; //binary: 0101...
+	static const uint64_t m2  = 0x3333333333333333; //binary: 00110011..
+	static const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
+	static const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
+	x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
+  x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
+  x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
+  return (x * h01)>>56;  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
+}
+
+#if !defined(_WIN64)
+static inline int _BitScanForward64(unsigned long *idx, uint64_t value) {
+	if (_BitScanForward(idx, value & 0xffffffff))
+		return 1;
+	if (_BitScanForward(idx, value >> 32)) {
+		*idx += 32;
+		return 1;
+	}
+	return 0;
+}
+
+static inline int _BitScanReverse64(unsigned long *idx, uint64_t value) {
+	if (_BitScanReverse(idx, value >> 32))
+		return 1;
+	if (_BitScanReverse(idx, value & 0xffffffff)) {
+		*idx += 32;
+		return 1;
+	}
+	return 0;
+}
+#endif
+
+static __inline int clz32(uint32_t value) {
+  unsigned long idx;
+  _BitScanReverse(&idx, value);
+  return idx;
+}
+
+static __inline int clz64(uint64_t value) {
+  unsigned long idx;
+  _BitScanReverse64(&idx, value);
+  return idx;
+}
+
+static __inline int fls32(uint32_t value) {
+  unsigned long idx = 0;
+  if (_BitScanReverse(&idx, value))
+    return idx + 1; // [1; 32] with 1 least significant bit
+  return 0;
+}
+static __inline int fls64(uint64_t value) {
+  unsigned long idx = 0;
+  if (_BitScanReverse64(&idx, value))
+    return idx + 1; // [1; 64] with 1 least significant bit
+  return 0;
+}
+
+static __inline int ctz32(uint32_t value) {
+  unsigned long idx; // do not init to match undefined behavior of GCC
+  _BitScanForward(&idx, value);
+  return idx;
+}
+static __inline int ctz64(uint64_t value) {
+  unsigned long idx;
+  _BitScanForward64(&idx, value);
+  return idx;
+}
+
+static __inline int ffs32(uint32_t value) {
+  unsigned long idx = 0;
+  if (_BitScanForward(&idx, value))
+    return idx + 1; // [1; 32] with 1 least significant bit
+  return 0;
+}
+static __inline int ffs64(uint64_t value) {
+  unsigned long idx = 0;
+  if (_BitScanForward64(&idx, value))
+    return idx + 1; // [1; 64] with 1 least significant bit
+  return 0;
+}
+
+#else
+// Population count
+SC_INLINE int popcount32(uint32_t value) { return __builtin_popcount(value); }
+SC_INLINE int popcount64(uint64_t value) { return __builtin_popcountll(value); }
+
+// Count Leading Zeros (left)
+// returns 0 based index. If value zero, result is undefined.
+SC_INLINE int clz32(uint32_t value) { return __builtin_clz(value); }
+SC_INLINE int clz64(uint64_t value) { return __builtin_clzll(value); }
+// returns 1 based index. If vlaue is 0, returns 0.
+SC_INLINE int fls32(uint32_t value) { return value ? __builtin_clz(value) + 1 : 0; }
+SC_INLINE int fls64(uint64_t value) { return value ? __builtin_clzll(value) + 1 : 0; }
+
+// Count Trailing Zeros (right)
+// returns 0 based index. If value zero, result is undefined.
+SC_INLINE int ctz32(uint32_t value) { return __builtin_ctz(value); }
+SC_INLINE int ctz64(uint64_t value) { return __builtin_ctzll(value); }
+// returns 1 based index. If vlaue is 0, returns 0.
+SC_INLINE int ffs32(uint32_t value) { return __builtin_ffs(value); }
+SC_INLINE int ffs64(uint64_t value) { return __builtin_ffsll(value); }
+
+// Returns the parity of value, i.e. the number of 1-bits in x modulo 2.
+//SC_INLINE int parity32(uint32_t value) { return __builtin_parity(value); }
+//SC_INLINE int parity64(uint64_t value) { return __builtin_parityll(value); }
+
+#endif
+
 #endif /* __WBC_BITFIELDS_H__ */
