cf474dd3eb71452bdf4ea36f1e56568b727b46f1
Jean-Daniel Dupas <jd.dupas@ninsight.com>
2011-08-26 19:27:59 +0200

    Move Obj-C Static Analyzer macros in objc.h

--- wbc/objc.h
+++ wbc/objc.h
@@ -15,6 +15,64 @@
 
 #if defined (__OBJC__)
 
+#ifndef NS_UNUSED_IVAR
+  #if __has_extension(attribute_objc_ivar_unused)
+    #define NS_UNUSED_IVAR __attribute__((unused))
+  #else
+    #define NS_UNUSED_IVAR
+  #endif
+#endif
+
+#ifndef NS_CONSUMED
+  #if __has_attribute(ns_consumed)
+    #define NS_CONSUMED __attribute__((ns_consumed))
+  #else
+    #define NS_CONSUMED
+  #endif
+#endif
+
+#ifndef NS_CONSUMES_SELF
+  #if __has_attribute(ns_consumes_self)
+    #define NS_CONSUMES_SELF __attribute__((ns_consumes_self))
+  #else
+    #define NS_CONSUMES_SELF
+  #endif
+#endif
+
+#ifndef NS_RETURNS_RETAINED
+  #if __has_attribute(ns_returns_retained)
+    #define NS_RETURNS_RETAINED __attribute__((ns_returns_retained))
+  #else
+    #define NS_RETURNS_RETAINED
+  #endif
+#endif
+
+#ifndef NS_RETURNS_NOT_RETAINED
+  #if __has_attribute(ns_returns_not_retained)
+    #define NS_RETURNS_NOT_RETAINED __attribute__((ns_returns_not_retained))
+  #else
+    #define NS_RETURNS_NOT_RETAINED
+  #endif
+#endif
+
+#ifndef NS_RETURNS_AUTORELEASED
+  #if __has_attribute(ns_returns_autoreleased)
+    #define NS_RETURNS_AUTORELEASED __attribute__((ns_returns_autoreleased))
+  #else
+    #define NS_RETURNS_AUTORELEASED
+  #endif
+#endif
+
+/* Method Family */
+#ifndef NS_METHOD_FAMILY
+  /* supported families are: none, alloc, copy, init, mutableCopy, and new. */
+  #if __has_attribute(ns_returns_autoreleased)
+    #define NS_METHOD_FAMILY(family) __attribute__((objc_method_family(family)))
+  #else
+    #define NS_METHOD_FAMILY(arg)
+  #endif
+#endif
+
 #if !__has_feature(objc_arc)
 /* Objective-C ARC keywords */
   #if !defined(__bridge)
