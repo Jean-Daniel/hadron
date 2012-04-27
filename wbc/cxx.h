7961d05a5304f1081d1806db285b9207c6b14ce1
Jean-Daniel Dupas <jddupas@xooloo.com>
2012-04-27 18:17:40 +0200

    Import all traits supported by libstdc++

--- wbc/cxx.h
+++ wbc/cxx.h
@@ -31,25 +31,81 @@
     template <class _If, class _Then>
     struct SC_VISIBLE conditional<false, _If, _Then> {typedef _Then type;};
 
-    // include common traits
-    using tr1::is_void;
-    using tr1::is_integral;
-    using tr1::is_floating_point;
-    using tr1::is_array;
-    using tr1::is_pointer;
-    using tr1::is_member_object_pointer;
-    using tr1::is_member_function_pointer;
-    using tr1::is_enum;
-    using tr1::is_union;
-    using tr1::is_class;
-    using tr1::is_function;
+		/// @brief  helper classes [4.3].
+    using tr1::integral_constant;
+		using tr1::true_type;
+		using tr1::false_type;
 
-    using tr1::is_signed;
-    using tr1::is_unsigned;
-    using tr1::is_arithmetic;
+		/// @brief  primary type categories [4.5.1].
+		using tr1::is_void;
+		using tr1::is_integral;
+		using tr1::is_floating_point;
+		using tr1::is_array;
+		using tr1::is_pointer;
+		using tr1::is_reference;
+		using tr1::is_member_object_pointer;
+		using tr1::is_member_function_pointer;
+		using tr1::is_enum;
+		using tr1::is_union;
+		using tr1::is_class;
+		using tr1::is_function;
 
-    using tr1::is_const;
-    using tr1::is_volatile;
+		/// @brief  composite type traits [4.5.2].
+		using tr1::is_arithmetic;
+		using tr1::is_fundamental;
+		using tr1::is_object;
+		using tr1::is_scalar;
+		using tr1::is_compound;
+		using tr1::is_member_pointer;
+
+		/// @brief  type properties [4.5.3].
+		using tr1::is_const;
+		using tr1::is_volatile;
+		using tr1::is_pod;
+		using tr1::is_empty;
+		using tr1::is_polymorphic;
+		using tr1::is_abstract;
+		using tr1::has_trivial_constructor;
+		using tr1::has_trivial_copy;
+		using tr1::has_trivial_assign;
+		using tr1::has_trivial_destructor;
+		using tr1::has_nothrow_constructor;
+		using tr1::has_nothrow_copy;
+		using tr1::has_nothrow_assign;
+		using tr1::has_virtual_destructor;
+		using tr1::is_signed;
+		using tr1::is_unsigned;
+		using tr1::alignment_of;
+		using tr1::rank;
+		using tr1::extent;
+
+		/// @brief  relationships between types [4.6].
+    using tr1::is_same;
+    using tr1::is_convertible;
+    using tr1::is_base_of;
+
+		/// @brief  const-volatile modifications [4.7.1].
+		using tr1::remove_const;
+		using tr1::remove_volatile;
+		using tr1::remove_cv;
+		using tr1::add_const;
+		using tr1::add_volatile;
+		using tr1::add_cv;
+
+		/// @brief  reference modifications [4.7.2].
+		using tr1::remove_reference;
+		using tr1::add_reference;
+
+		/// @brief  array modifications [4.7.3].
+		using tr1::remove_extent;
+		using tr1::remove_all_extents;
+
+		/// @brief  pointer modifications [4.7.4].
+		using tr1::remove_pointer;
+		using tr1::add_pointer;
+
+		/// @brief  other transformations [4.8].
+		using tr1::aligned_storage;
   }
   #endif
 #endif
