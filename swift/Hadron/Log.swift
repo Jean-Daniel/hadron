f61d8777df26e45fe6d296b20b57b4c26590da67
Jean-Daniel <jean-daniel@xenonium.com>
2018-10-04 10:21:52 +0200

    OSLog is the new way to log output.

--- swift/Hadron/Log.swift
+++ swift/Hadron/Log.swift
@@ -6,136 +6,76 @@
 //  Copyright (c) 2014 Xenonium. All rights reserved.
 //
 
+import os
 import Foundation
 
-@_silgen_name("pthread_mach_thread_np")
-private func pthread_mach_thread_np(_ : pthread_t) -> mach_port_t;
+import os.activity
 
-// This hacky call is from CoreFoundation.
-// This is the way CoreFoundation (and so NSLog) determines if it should log in stderr
-// As we only use stderr (and not asl like CF does), we use this same hack to prevent duplicate
-// prefix in Console output when not running in Xcode (launchd already append a prefix).
-// if it could be a pipe back to launchd, don't prepend a prefix
-@discardableResult
-@_silgen_name("vproc_swap_integer")
-private func _vproc_swap_integer(_: OpaquePointer?, _: CInt, _: UnsafePointer<Int64>?, _: UnsafePointer<Int64>?) -> OpaquePointer?;
+let i = _create_ac
 
-typealias asl_object_t = OpaquePointer?;
-@_silgen_name("asl_vlog")
-private func asl_log(_ client: asl_object_t, msg: asl_object_t, level : Int32, fmt : UnsafePointer<Int8>!, args : CVaListPointer);
-
-final class Logger {
-  
-  enum Level : Int32 {
-    case emergency,
-    alert,
-    critical,
-    error,
-    warning,
-    notice,
-    info,
-    debug
-  }
-
-  // MARK: Convenient methods
-  func warn(_ message : String, client: asl_object_t = nil, msg: asl_object_t = nil) {
-    log(level: .warning, message: message, client: client, msg: msg);
-  }
-  
-  func error(_ message : String, client: asl_object_t = nil, msg: asl_object_t = nil) {
-    log(level: .error, message: message, client: client, msg: msg);
-  }
-  
-  // MARK: =================== Standard Logging ===================
-  #if DEBUG
-  func debug(message : String) {
-    if Logger.hasPrefix {
-      var nows = timeval(tv_sec: 0, tv_usec: 0);
-      gettimeofday(&nows, nil);
-      let date = Logger.format.string(from: Date(timeIntervalSince1970:TimeInterval(nows.tv_sec)));
-      "\(date).%.3u \(Logger.progname)[\(getpid()):%x] %s\n".withCString { ( fmt : UnsafePointer<Int8>) -> Void in
-        message.withCString {
-          withVaList([nows.tv_usec / 1000, pthread_mach_thread_np(pthread_self()), $0]) {
-            vfprintf(__stderrp, fmt, $0);
-            return;
-          }
-        }
-      }
-    } else {
-      "%s\n".withCString { (str : UnsafePointer<Int8>) -> () in
-        message.withCString {
-          withVaList([$0]) { vfprintf(__stderrp, str, $0); } as ()
-        }
-      }
-    }
-  }
-
-  func trace(args: String, file : String = #file, line : Int = #line, function : String = #function) {
-    debug(message: "[\((file as NSString).lastPathComponent):\(line)]: \(function) (\(args))\n");
-  }
-  
-  func log(level : Level, message : String, client: asl_object_t, msg: asl_object_t) {
-    debug(message: "Log(\(level): \(message)");
-  }
-  
-  #else
-  // MARK: Release
-  // Instance Methods
-  func debug(_ message : @autoclosure () -> String) {}
-  
-  func log(level : Level, message : String, client: asl_object_t, msg: asl_object_t) {
-    "%s".withCString { (fmt : UnsafePointer<Int8>) -> Void in
-      message.withCString {
-        withVaList([$0]) {
-          asl_log(client, msg: msg, level: level.rawValue, fmt: fmt, args: $0);
-        }
-      }
-    }
-  }
-  #endif
-
-  #if DEBUG
-  private static let progname = String(validatingUTF8:getprogname()) ?? "<?>"
-  private static let hasPrefix : Bool = {
-    var val : Int64 = 0;
-    _vproc_swap_integer(nil, 5 /* VPROC_GSK_IS_MANAGED */, nil, &val);
-    return val <= 0;
-  }();
-
-  private static let format : DateFormatter = {
-    let fmt = DateFormatter()
-    fmt.dateFormat = "yyyy-MM-dd HH:mm:ss";
-    fmt.locale = Locale(identifier:"en_US");
-    return fmt;
-  }()
-  #endif
+@inline(__always)
+func spx_info(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString) {
+  os_log(message, dso: dso, log: log, type: .info)
+}
+@inline(__always)
+func spx_info(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString, _ args: CVarArg) {
+  os_log(message, dso: dso, log: log, type: .info, args)
+}
+@inline(__always)
+func spx_info(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString, _ args1: CVarArg, _ args2: CVarArg) {
+  os_log(message, dso: dso, log: log, type: .info, args1, args2)
 }
 
-private let _logger = Logger();
-
-func spx_log(_ level : Logger.Level, message : String, client: asl_object_t = nil, msg: asl_object_t = nil) {
-  _logger.log(level: level, message: message, client: client, msg: msg);
+@inline(__always)
+func spx_log(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString) {
+  os_log(message, dso: dso, log: log, type: .default)
+}
+@inline(__always)
+func spx_log(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString, _ args: CVarArg) {
+  os_log(message, dso: dso, log: log, type: .default, args)
+}
+@inline(__always)
+func spx_log(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString, _ args1: CVarArg, _ args2: CVarArg) {
+  os_log(message, dso: dso, log: log, type: .default, args1, args2)
 }
 
-func spx_warn(_ message : String, client: asl_object_t = nil, msg: asl_object_t = nil) {
-  _logger.warn(message, client: client, msg: msg);
+@inline(__always)
+func spx_error(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString) {
+  os_log(message, dso: dso, log: log, type: .error)
 }
-
-func spx_error(_ message : String, client: asl_object_t = nil, msg: asl_object_t = nil) {
-  _logger.error(message, client: client, msg: msg);
+@inline(__always)
+func spx_error(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString, _ args: CVarArg) {
+  os_log(message, dso: dso, log: log, type: .error, args)
+}
+@inline(__always)
+func spx_error(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString, _ args1: CVarArg, _ args2: CVarArg) {
+  os_log(message, dso: dso, log: log, type: .error, args1, args2)
 }
 
 #if DEBUG
-  func spx_debug(_ message : String) {
-    _logger.debug(message: message);
-  }
+@inline(__always)
+func spx_debug(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString) {
+  os_log(message, dso: dso, log: log, type: .debug)
+}
+@inline(__always)
+func spx_debug(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString, _ args: CVarArg) {
+  os_log(message, dso: dso, log: log, type: .debug, args)
+}
+@inline(__always)
+func spx_debug(dso: UnsafeRawPointer = #dsohandle, log: OSLog = OSLog.default, _ message: StaticString, _ args1: CVarArg, _ args2: CVarArg) {
+  os_log(message, dso: dso, log: log, type: .debug, args1, args2)
+}
 
-  func spx_trace(args: String = "", file : String = #file, line : Int = #line, function : String = #function) {
-    _logger.trace(args: args, file: file, line: line, function: function);
-  }
+@inline(__always)
+func spx_trace(args: String = "", file : String = #file, line : Int = #line, function : String = #function, dso: UnsafeRawPointer? = #dsohandle) {
+  let filename = (file as NSString).lastPathComponent
+  os_log("[%@:%d]: %@ (%@)", dso: dso, log: OSLog.default, type: .debug, filename, line, function, args)
+}
 #else
-  // MARK: Release
-  func spx_debug(_ message : @autoclosure () -> String) { /* noop */  }
+// MARK: Release
+@inline(__always)
+func spx_debug(log: OSLog = OSLog.default, _ message: StaticString, _ args: CVarArg...) { /* noop */  }
 
-  func spx_trace(args: @autoclosure () -> String = "") { /* Noop */ }
+@inline(__always)
+func spx_trace(args: @autoclosure () -> String = "") { /* Noop */ }
 #endif
