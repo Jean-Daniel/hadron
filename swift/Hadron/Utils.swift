2039170ab2e79f9e97968feb91f5c5b969115c30
Jean-Daniel Dupas <jddupas@xooloo.com>
2018-11-08 17:33:15 +0000

    Update xcconfig to fix framwork code sign and application rpath.

--- swift/Hadron/Utils.swift
+++ swift/Hadron/Utils.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Xenonium. All rights reserved.
 //
 
+@inline(__always)
 func with<T>(_ object: T, update: (inout T) throws -> (Void)) rethrows -> T {
   var obj = object
   try update(&obj)
