1d300a23182c82951a5d797f499431bd2bfda5c1
Jean-Daniel Dupas <jddupas@xooloo.com>
2018-11-09 16:14:03 +0000

    Add LocalizedString 'macro' that automatically resolve the right Bundle.

--- swift/Hadron/Utils.swift
+++ swift/Hadron/Utils.swift
@@ -6,9 +6,22 @@
 //  Copyright © 2016 Xenonium. All rights reserved.
 //
 
+import Foundation
+
 @inline(__always)
 func with<T>(_ object: T, update: (inout T) throws -> (Void)) rethrows -> T {
   var obj = object
   try update(&obj)
   return obj
 }
+
+@objc
+class _BundleAnchor : NSObject {}
+
+private let _self_bundle : Bundle = Bundle(for: _BundleAnchor.self)
+
+@inline(__always)
+func LocalizedString(_ key: String, tableName: String? = nil, comment: String) -> String {
+  return NSLocalizedString(key, tableName: tableName, bundle: _self_bundle, comment: comment)
+}
+
